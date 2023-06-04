b2916bfd7390d621dcb412410131967d7837e4dc
kushtarbek <dr.kushtarbek@gmail.com>
2023-06-03 23:15:12 -0500

    genetic stack with primitive example

---
+++ Stack/Primitives.java
@@ -0,0 +1,20 @@
+package Stack;
+/**
+ * Each primitive type has a wrapper object type
+ * Integer is wrapper type for int;
+ *
+ * Autoboxing: Automatic cast between a primitive type and its wrapper.
+ * Syntactic sugar: Behind-the-scenes casting.
+ *
+ * Bottom line: Client can use generic stack for any type of data.
+ */
+
+
+public class Primitives {
+    Stack <Integer> s = new Stack();
+    s.push(17);
+    int a = s.pop();
+    public static void main(String[] args) {
+
+    }
+}
