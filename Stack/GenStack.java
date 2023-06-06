b798afd4bb3dd9775496719a89068d9908813367
kushtarbek <dr.kushtarbek@gmail.com>
2023-06-05 23:31:48 -0500

    complete code of stack of strings

--- Stack/GenStack.java
+++ Stack/GenStack.java
@@ -33,5 +33,20 @@ public class GenStack<E> {
         gs.push("Two");
         gs.push("Three");
         gs.push("Four");
+
+        System.out.println("There are "+ gs.size() + " items in the stack. \n");
+
+        System.out.println("The top item is: "+gs.peek()+"\n");
+
+        System.out.println("There are still "+ gs.size() + " items in the stack. \n");
+
+        System.out.println("Popping everything: ");
+        while(gs.hasItems()){
+            System.out.println(gs.pop());
+        }
+
+        System.out.println("There are now "+gs.size()+" items in the stack.\n");
+
+        System.out.println("The top item is: "+gs.peek()+"\n");
     }
 }
--- Stack/StackOfStrings.java
+++ Stack/StackOfStrings.java
@@ -2,11 +2,11 @@ package Stack;
 
 public class StackOfStrings {
 //    public static void main(String[] args) {
-        StackOfStrings stack = new StackOfStrings();
-        while( !StdIn.isEmpty()) {
-            String s = StdIn.readString();
-            if(s.equals("-")) StdOut.print(stack.pop());
-            else    stack.push(s);
-        }
+//        StackOfStrings stack = new StackOfStrings();
+//        while( !StdIn.isEmpty()) {
+//            String s = StdIn.readString();
+//            if(s.equals("-")) StdOut.print(stack.pop());
+//            else    stack.push(s);
+//        }
     //}
 }
