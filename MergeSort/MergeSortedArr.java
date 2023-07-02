4ca11c3d8979b93400b1345c6aa31ec021026fc4
kushtarbek <dr.kushtarbek@gmail.com>
2023-07-01 23:51:01 -0500

    LC_88

---
+++ MergeSort/MergeSortedArr.java
@@ -0,0 +1,20 @@
+package MergeSort;
+
+public class MergeSortedArr {
+        public void merge(int[] nums1, int m, int[] nums2, int n) {
+            int i = m - 1;
+            int j = nums1.length - 1;
+            int p2 = n - 1;
+            while (p2 >= 0) {
+                if (i >= 0 && nums1[i] > nums2[p2]) {
+                    nums1[j--] = nums1[i--];
+                } else {
+                    nums1[j--] = nums2[p2--];
+                }
+            }
+        }
+
+    public static void main(String[] args) {
+
+    }
+}
