7556c5d0ee271097d2eea91e23e6bab57ba75d52
kushtarbek <dr.kushtarbek@gmail.com>
2023-07-02 23:25:07 -0500

    trying to solve outside of LC

---
+++ Array/TwoSum.java
@@ -0,0 +1,20 @@
+package Array;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSum {
+    public static int [] twoSum( int [] nums, int target){
+        HashMap <Integer, Integer> map = new HashMap<>();
+        for ( int i=0; i< nums.length; i++){
+            if(map.containsKey(target - nums[i])){
+                return new int [] {map.get(target - nums[i]),i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int []{};
+    }
+    public static void main(String[] args) {
+    int [] arr = {2,7,11,15};
+    System.out.println(Arrays.toString(twoSum(arr, 9)));
+    }
+}
