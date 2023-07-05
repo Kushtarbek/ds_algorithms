db73ea6d5ff4f08b4227f175f141392918836635
kushtarbek <dr.kushtarbek@gmail.com>
2023-07-04 23:36:43 -0500

    LC_1920

---
+++ Stack/ValidParenthesis.java
@@ -0,0 +1,20 @@
+package Stack;
+
+import java.util.Stack;
+
+public class ValidParenthesis {
+    public static boolean isValid(String s) {
+        Stack <Character> stack = new Stack<Character>();
+        for( char c : s.toCharArray()){
+            if( c == '(') stack.push(')');
+            else if( c == '[') stack.push( ']');
+            else if( c == '{') stack.push( '}');
+            else if( stack.isEmpty() || stack.pop() != c) return false;
+        }
+
+        return stack.isEmpty();
+    }
+    public static void main(String[] args) {
+    char arr =
+    }
+}
