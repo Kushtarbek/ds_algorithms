ae6cac6ea12a72bf335137249d6ddf40e7ca3d4b
kushtarbek <dr.kushtarbek@gmail.com>
2023-07-12 23:29:37 -0500

    happy number

---
+++ HashMap/happynum.java
@@ -0,0 +1,24 @@
+package HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class happynum {
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            int sum = 0;
+            while (n != 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+            n = sum;
+        }
+        return n == 1;
+    }
+    public static void main(String[] args) {
+
+    }
+}
