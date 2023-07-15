7ec31e3ee2367dd5d13acdae71f5ffe286ac052a
kushtarbek <dr.kushtarbek@gmail.com>
2023-07-14 23:51:54 -0500

    behavioral questions

--- HashMap/TwoSumHashMap.java
+++ HashMap/TwoSumHashMap.java
@@ -1,6 +1,26 @@
 package HashMap;
+
 public class TwoSumHashMap {
+    /*
+     * Template for using hash map to find duplicates.
+     * Replace ReturnType with the actual type of your return value.
+     */
+//    ReturnType aggregateByKey_hashmap(List<Type>& keys) {
+//        // Replace Type and InfoType with actual type of your key and value
+//        Map<Type, InfoType> hashmap = new HashMap<>();
+//        for (Type key : keys) {
+//            if (hashmap.containsKey(key)) {
+//                if (hashmap.get(key) satisfies the requirement) {
+//                    return needed_information;
+//                }
+//            }
+//            // Value can be any information you needed (e.g. index)
+//            hashmap.put(key, value);
+//        }.
+//        return needed_information;
+//    }
     public static void main(String[] args) {
-        
+
+
     }
 }
