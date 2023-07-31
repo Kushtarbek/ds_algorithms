c60bb0ea21d207b549c02cdd435d7a18ccb6157d
kushtarbek <dr.kushtarbek@gmail.com>
2023-07-30 22:22:15 -0500

    hashset LC_128

---
+++ HashMap/LongestConsecutiveSequence.java
@@ -0,0 +1,23 @@
+package HashMap;
+
+import java.util.HashSet;
+
+public class LongestConsecutiveSequence {
+    public int longestConsecutive(int[] nums) {
+        if(nums.length == 0 ) return 0;
+        HashSet<Integer> set = new HashSet<>();
+        int ans = 1;
+        for( int num : nums) set.add(num);
+        for( int num : nums){
+            if(!set.contains(num-1)){
+                int count = 1;
+                while(set.contains(num+1)){
+                    num++;
+                    count++;
+                }
+                ans = Math.max(count, ans);
+            }
+        }
+        return ans;
+    }
+}
