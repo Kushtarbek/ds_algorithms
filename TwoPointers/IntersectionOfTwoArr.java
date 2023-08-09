fbea3e1110fe8cbfcdb8977384b4dbd2cc03cc3b
kushtarbek <dr.kushtarbek@gmail.com>
2023-08-08 22:58:58 -0500

    two hashset method;

--- TwoPointers/IntersectionOfTwoArr.java
+++ TwoPointers/IntersectionOfTwoArr.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 public class IntersectionOfTwoArr {
-   public int[] intersection( int[] nums1, int[] nums2){
+
+   public static int[] intersection(int[] nums1, int[] nums2){
        Arrays.sort(nums1);
        Arrays.sort(nums2);
 
@@ -34,3 +35,25 @@ public class IntersectionOfTwoArr {
        return finalRes;
    }
 }
+
+// HashSet<Integer> seen = new HashSet<>();
+// HashSet<Integer> inter = new HashSet<>();
+
+// for (int i=0; i< nums1.length; i++ ) {
+//     if(!seen.contains(nums1[i])){
+//         seen.add(nums1[i]);
+//     }
+// }
+
+// for (int i=0; i < nums2.length; i++) {
+//     if(seen.contains(nums2[i])){
+//         inter.add(nums2[i]);
+//     }
+// }
+// int n = inter.size();
+// int arr[] = new int[n];
+
+// int i=0;
+// for(int x : inter)
+//     arr[i++] = x;
+// return arr;
