29d3ab7165c4800813a01b24fc1bff61c62a99f2
kushtarbek <dr.kushtarbek@gmail.com>
2023-08-17 23:27:19 -0500

    LC_11

---
+++ TwoPointers/ContainerWithMostWater.java
@@ -0,0 +1,24 @@
+package TwoPointers;
+
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int l = 0;
+        int r = height.length-1;
+        int max = 0;
+        while(l< r ){
+            int w = r - l;
+            int h =  Math.min(height[l], height[r]);
+            int area  = h * w;
+            max = Math.max(max, area);
+            if(height[l] < height[r]){
+                l++;
+            }else if(height[l] > height[r]){
+                r--;
+            }else{
+                l++;
+                r--;
+            }
+        }
+        return max;
+    }
+}
