b307525c89386639daeb8c6fd7c31cd87004ff3d
kushtarbek <dr.kushtarbek@gmail.com>
2023-08-17 23:33:43 -0500

    another way of two pointers solution

--- TwoPointers/ContainerWithMostWater.java
+++ TwoPointers/ContainerWithMostWater.java
@@ -27,3 +27,18 @@ public class ContainerWithMostWater {
 Time complexity: O(N)
 Space complexity: O(1)
 */
+
+//    public int maxArea(int[] H) {
+//        int ans = 0, i = 0, j = H.length-1, res = 0;
+//        while (i < j) {
+//            if (H[i] <= H[j]) {
+//                res = H[i] * (j - i);
+//                i++;
+//            } else {
+//                res = H[j] * (j - i);
+//                j--;
+//            }
+//            if (res > ans) ans = res;
+//        }
+//        return ans;
+//    }
